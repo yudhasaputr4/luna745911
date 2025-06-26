173803c86c7a11e89de622cbe807c1225bff7db5
yudhasaputr4 <yssaputraa@gmail.com>
2025-06-27 04:19:27 +0800

    aaa

--- client/src/components/chat.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Chat() {
-  return (
-    <div className="flex flex-col items-center justify-center h-full">
-      <div className="text-lg font-semibold">Chat component coming soon...</div>
-    </div>
-  );
-}
